d3c209d7d1574579587520fbd2aa9c41a3aeba15
Kxxcelo1 <207324420+Kingsleyxelo@users.noreply.github.com>
2026-04-13 22:52:01 +0200

    Practice

--- src/Overriding.java
+++ src/Overriding.java
@@ -1,4 +1,29 @@
+class mammal {
+    void move(){
+        System.out.println("Animal is moving");
+    }
+    void eat(){
+        System.out.println("Animal is eating");
+    }
+}
+class Dog extends mammal {
+     void move(){
+        //super.move();
+        System.out.println("Dog is running");
+    }
 
+     void bark(){
+        System.out.println("Dog barks");
+    }
+}
 
 public class Overriding {
+    public static void main(String[] args) {
+        mammal obj1 = new mammal();
+        obj1.move();
+        //obj1.bark();
+        obj1.eat();
+        Dog obj2 = new Dog();
+        obj2.bark();
+    }
 }
